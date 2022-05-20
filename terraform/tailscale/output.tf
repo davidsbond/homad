2edbe362705a2f5f2103bb2f87b61b4d990b9cf2
David Bond <davidsbond93@gmail.com>
2022-05-20 14:56:50 +0100

    Automatically add new nomad clients
    This commit modifies the CI pipeline to reapply the configuration every hour,
    it also changes the tailscale query to look up all devices with a "homad-"
    prefix and add them to the DNS records. This should mean that when new clients
    are added, provided they use the correct naming format, will automatically have
    their DNS records set up every hour.
    
    This should just work as the traefik deployment is set up as a system job, so
    each client will get a traefik instance once they're ready.
    
    Signed-off-by: David Bond <davidsbond93@gmail.com>

--- terraform/tailscale/output.tf
+++ terraform/tailscale/output.tf
@@ -1,8 +1,9 @@
+# homad-0 is the server node, so it won't be serving any traffic, we want to get all tailscale devices whose
+# names are prefixed with "homad-", filter out "homad-0" and return the first address for each node.
 output "homelab_clients" {
   value = [
-    element(data.tailscale_device.homad_1.addresses, 0),
-    element(data.tailscale_device.homad_2.addresses, 0),
-    element(data.tailscale_device.homad_3.addresses, 0),
+    for client in data.tailscale_devices.nomad_clients.devices : element(client.addresses, 0)
+    if client.name != "homad-0.davidsbond93.gmail.com"
   ]
 }
 
