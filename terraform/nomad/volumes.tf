a7ed4e9cf2996e995087c8cbd9b6eb8222abe9d4
David Bond <davidsbond@users.noreply.github.com>
2022-05-27 14:42:56 +0100

    Add volumes for services

---
+++ terraform/nomad/volumes.tf
@@ -0,0 +1,53 @@
+resource "nomad_external_volume" "home_assistant" {
+  type         = "csi"
+  plugin_id    = "nfs"
+  volume_id    = "home-assistant"
+  name         = "home-assistant"
+  capacity_min = "10M"
+  capacity_max = "1Gi"
+
+  capability {
+    access_mode     = "multi-node-multi-writer"
+    attachment_mode = "file-system"
+  }
+
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+resource "nomad_external_volume" "bitwarden" {
+  type         = "csi"
+  plugin_id    = "nfs"
+  volume_id    = "bitwarden"
+  name         = "bitwarden"
+  capacity_min = "10M"
+  capacity_max = "1Gi"
+
+  capability {
+    access_mode     = "multi-node-multi-writer"
+    attachment_mode = "file-system"
+  }
+
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+resource "nomad_external_volume" "postgres" {
+  type         = "csi"
+  plugin_id    = "nfs"
+  volume_id    = "postgres"
+  name         = "postgres"
+  capacity_min = "10M"
+  capacity_max = "1Gi"
+
+  capability {
+    access_mode     = "multi-node-multi-writer"
+    attachment_mode = "file-system"
+  }
+
+  lifecycle {
+    prevent_destroy = false
+  }
+}
