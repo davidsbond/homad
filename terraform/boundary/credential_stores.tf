20c554d187a52bd31afdeafe6fd0399e1759b9c2
David Bond <davidsbond@users.noreply.github.com>
2022-07-05 15:27:42 +0100

    More boundary configuration (#50)
    I did this a while ago and can't remember what I was doing with it. I've modified
    the boundary configuration to set the worker ip addres correctly and attempted
    to set up vault as a credential library.
    
    Signed-off-by: David Bond <davidsbond93@gmail.com>

--- terraform/boundary/credential_stores.tf
+++ terraform/boundary/credential_stores.tf
@@ -4,3 +4,10 @@ resource "boundary_credential_store_vault" "homad" {
   token    = var.vault_token
   scope_id = boundary_scope.homad.id
 }
+
+resource "boundary_credential_library_vault" "homad" {
+  name                = "vault"
+  credential_store_id = boundary_credential_store_vault.homad.id
+  path                = "boundary/homad"
+  http_method         = "GET"
+}
