4f5f454ec9b7e4a375c40b91969d7f0769b9af77
David Bond <davidsbond93@gmail.com>
2022-08-01 14:38:33 +0100

    Validate nomad jobs with connection to the agent
    This commit modifies the nomad job validation step to connect to Tailscale and
    set the `NOMAD_ADDR` environment variable so that we get full job validation
    for the driver configuration.
    
    Signed-off-by: David Bond <davidsbond93@gmail.com>

--- scripts/upgrade-ubuntu.sh
+++ scripts/upgrade-ubuntu.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+# This script is used on Nomad clients/servers to keep the underlying OS and its packages up-to-date. This script
+# should be ran on a schedule by Nomad itself.
 
 set -e
 
--- scripts/validate-jobs.sh
+++ scripts/validate-jobs.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+# This script is used to validate all Nomad jobs stored within this repository. When available, the NOMAD_ADDR
+# environment variable should be set to allow validation of driver configuration. Driver configuration is only
+# available if the Nomad agent is also available.
 
 set -e
 
