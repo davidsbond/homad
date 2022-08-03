059d643583b0c2a617656ce11e0c5c68e5f5a298
David Bond <davidsbond93@gmail.com>
2022-08-03 16:26:56 +0100

    Create backup job for postgres instance
    This commit contains an hourly job that will backup all non-system databases within
    the postgres instance using `pg_dump` and write the gzipped contents to a bucket
    in minio.
    
    The job is not currently terraformed for initial testing.
    
    Signed-off-by: David Bond <davidsbond93@gmail.com>

---
+++ scripts/backup-database.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# This script is used to write the contents of a postgres database to blob storage for backups.
+
+set -e
+
+DATABASES=$(psql -t -c "SELECT datname FROM pg_database WHERE datname NOT IN ('template0', 'template1', 'postgres')")
+
+mc alias set minio https://api.minio.homelab.dsb.dev "$MINIO_ACCESS_KEY" "$MINIO_SECRET_KEY"
+
+for DATABASE in $DATABASES; do
+  BUCKET_PATH=minio/postgres-backups/"$DATABASE".sql.gz
+  pg_dump --host postgres.homelab.dsb.dev "$DATABASE" | gzip | mc pipe "$BUCKET_PATH"
+done
+
+
