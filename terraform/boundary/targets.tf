ab0afedec0fcd96be93b4fa43af72b1b789dad83
David Bond <davidsbond93@gmail.com>
2022-10-24 15:39:46 +0100

    Switch from deprecated boundary resources & update NAS name
    This commit removes the usage of deprecated resources in the boundary provider and updates
    the tailscale device name for my NAS as they've changed the name format.
    
    Signed-off-by: David Bond <davidsbond93@gmail.com>

--- terraform/boundary/targets.tf
+++ terraform/boundary/targets.tf
@@ -4,7 +4,7 @@ resource "boundary_target" "homelab" {
   default_port = "22"
   scope_id     = boundary_scope.homad.id
   host_source_ids = [
-    boundary_host_set.homelab.id,
+    boundary_host_set_static.homelab.id,
   ]
   brokered_credential_source_ids = [
     boundary_credential_library_vault.homad.id
--- terraform/boundary/users.tf
+++ terraform/boundary/users.tf
@@ -1,5 +1,5 @@
 resource "boundary_user" "david" {
   name        = "david"
-  account_ids = [boundary_account.david.id]
+  account_ids = [boundary_account_password.david.id]
   scope_id    = boundary_scope.org.id
 }
